9511feda292bcdc399a14a2a9bef250766c92093
Brenda Moreira <brendanatalia94@hotmail.com>
2020-04-23 14:12:39 +0200

    Create base for JS, Remove milliseconds

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-n
+let startButton = document.querySelector('.start');
+startButton.addEventListener('click', startStop);
+let second = document.querySelector('.seconds');
+let seconds = second.innerHTML;
+let minute = document.querySelector('.minutes');
+let minutes = minute.innerHTML;
+let hour = document.querySelector('.hours');
+let hours = hour.innerHTML
+function startStop() {
+    console.log('clicked');
+    setInterval(function(){
+        console.log('1second');
+    }, 1000);
+    setInterval(function(){
+        console.log('1minute');
+    }, 60000);
+    setInterval(function(){
+        console.log('')
+    }, 3600000)
+}
+let restartButton = document.querySelector('.restart');
+restartButton.addEventListener('click', function(event) {
+    console.log('clickedR')
+})
